6206eb102b268ed93e72446557d854029e5aeb2a
Jerome Illgner <ji8@hw.ac.uk>
2019-11-25 05:32:26 +0000

    Data loading (#18)
    * data loading done
    
    * Fixed loading function
    
    * Fixed warnings

--- network/__main__.py
+++ network/__main__.py
@@ -1,12 +1,25 @@
+import os
+from os import path
 import click
-
-from network.ANN import ANN
+import pandas
 
 
 @click.command()
-def network():
-    test = ANN([3, 2, 1])
-    print(test.weights)
+@click.argument('data_folder')
+def network(data_folder):
+    test = load_data(data_folder)
+    print(test)
+
+
+def load_data(folder: str):
+    data = {}
+    with click.progressbar(os.listdir(folder)) as bar:
+        for file in bar:
+            no_inputs = int(file[0])
+            frame = pandas.read_table(path.join(folder, file), delim_whitespace=True, header=None).to_numpy()
+            file = file[:-4]
+            data[file] = frame[:, :no_inputs], frame[:, no_inputs:]
+    return data
 
 
 if __name__ == "__main__":
